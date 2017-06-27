86840e490a0e725856662e88e625ab18a9c3afce
millems <millem@amazon.com>
2017-06-27 15:34:35 -0700

    Fixed list in javadoc. (#217)

--- core/src/main/java/software/amazon/awssdk/client/builder/ClientBuilder.java
+++ core/src/main/java/software/amazon/awssdk/client/builder/ClientBuilder.java
@@ -42,14 +42,14 @@ public interface ClientBuilder<B extends ClientBuilder<B, C>, C> extends SdkBuil
      * Configure the credentials that should be used to authenticate with AWS.
      *
      * <p>The default provider will attempt to identify the credentials automatically using the following checks:
-     * <ul>
+     * <ol>
      *   <li>Java System Properties - <code>aws.accessKeyId</code> and <code>aws.secretKey</code></li>
      *   <li>Environment Variables - <code>AWS_ACCESS_KEY_ID</code> and <code>AWS_SECRET_ACCESS_KEY</code></li>
      *   <li>Credential profiles file at the default location (~/.aws/credentials) shared by all AWS SDKs and the AWS CLI</li>
      *   <li>Credentials delivered through the Amazon EC2 container service if AWS_CONTAINER_CREDENTIALS_RELATIVE_URI environment
      *   variable is set and security manager has permission to access the variable.</li>
      *   <li>Instance profile credentials delivered through the Amazon EC2 metadata service</li>
-     * </ul>
+     * </ol>
      *
      * <p>If the credentials are not found in any of the locations above, an exception will be thrown at {@link #build()} time.
      * </p>
